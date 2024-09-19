362afdc86bdcd67304ea1169953a19a4b9b7e984
Krystian Chanek <krystian.chanek@amtsolution.pl>
2024-09-19 09:26:33 +0200

    Add extension configuration

--- Classes/Controller/SettingsController.php
+++ Classes/Controller/SettingsController.php
@@ -3,14 +3,23 @@
 namespace Amt\AmtPinecone\Controller;
 
 use Psr\Http\Message\ResponseInterface;
+use TYPO3\CMS\Core\Configuration\ExtensionConfiguration;
+use TYPO3\CMS\Core\Utility\GeneralUtility;
 
 class SettingsController extends BaseController
 {
     public function settingsAction(): ResponseInterface
     {
+        $configuration = GeneralUtility::makeInstance(ExtensionConfiguration::class)->get('amt_pinecone');
         $moduleTemplate = $this->createRequestModuleTemplate();
 
-        $moduleTemplate->assign('message', 'Hello - AmtPinecone Extension!');
+        $moduleTemplate->assignMultiple(
+            [
+                'openAiApiKey' => $configuration['openAiApiKey'],
+                'openAiModelForEmbeddings' => $configuration['openAiModelForEmbeddings'],
+                'pineocneApiKey' => $configuration['pineconeApiKey'],
+
+            ]);
 
         return $moduleTemplate->renderResponse('Settings');
     }
